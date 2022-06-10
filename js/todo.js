02dc4f35bddd633c7f07191c0693908f620235ea
breakndream <kang26d@mtholyoke.edu>
2022-06-10 18:16:34 +0900

    To do list

--- js/todo.js
+++ js/todo.js
@@ -4,60 +4,55 @@ const todoList = document.getElementById("todo-list");
 let toDoArray = [];
 const TODOS_KEY = "todos";
 
+
+todoForm.addEventListener("submit", submitToDo);
+
 function submitToDo(event) {
     event.preventDefault();
     const newTodo = todoInput.value;
-    toDoArray.push(newTodo);
     todoInput.value = "";
-    showTodo(newTodo)
+    const newTodoObject = {
+        text: newTodo,
+        id : Date.now(),
+    }
+    toDoArray.push(newTodoObject);
+    showTodo(newTodoObject)
     saveTodo();   
-}
-
-function showTodo(whatToDo) {
-    const li = document.createElement("li");
-
-    const span = document.createElement("span");
-    span.innerText = `${whatToDo} `;
-    
-    const btn = document.createElement("button");
-    btn.innerText = "❌";
-    btn.addEventListener("click", deleteTodo);
-
-    li.appendChild(span);
-    li.appendChild(btn);
-    todoList.appendChild(li);
-}
-
-function deleteTodo(event) {
-    const removeLi = event.target.parentElement;
-    console.log(removeLi);
-    //removeLi. id를 span first child의 innerText로 한 다음에 
-    removeLi.remove();
-  
 }
 
 function saveTodo(){
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDoArray));
 }
 
-todoForm.addEventListener("submit", submitToDo);
-
 const savedTodos =  localStorage.getItem(TODOS_KEY);
 
 if (savedTodos !== null) {
     const parsedTodos = JSON.parse(savedTodos);
-    // so 중요!!!!!!! 이거 안하면 reload 했을 때 날아감!!!!!!!!!
     toDoArray = parsedTodos;
-    //parameter 안에 item 자동 들어감
-    parsedTodos.forEach(showTodo)
-
-    /**
-     * function sayHello(item) {
-     * console.log("this is the turn of "+ item)
-     * }
-     * 
-     * parsedTodos.forEach(sayHello)와 동일
-     */
-    // parsedTodos.forEach((item) => console.log("this is the turn of ", item))
+    parsedTodos.forEach(showTodo);
 }
 
+function showTodo(whatToDo) {
+    const li = document.createElement("li");
+    // id 세팅하는 법
+    li.id = whatToDo.id;
+    const span = document.createElement("span");
+    span.innerText = `${whatToDo.text} `;
+    
+    const btn = document.createElement("button");
+    btn.innerText = "❌";
+
+    todoList.appendChild(li);
+    li.appendChild(span);
+    li.appendChild(btn);
+    
+    btn.addEventListener("click", deleteTodo);
+}
+
+function deleteTodo(event) {
+    const removeLi = event.target.parentElement;
+    toDoArray = toDoArray.filter((item) => item.id !== parseInt(removeLi.id));
+    //removeLi. id를 span first child의 innerText로 한 다음에 if 문 사용해서 같으면 해볼까? -> 이거 안됨 (aa 랑 a의 경우..)
+    removeLi.remove();
+    saveTodo();
+}
